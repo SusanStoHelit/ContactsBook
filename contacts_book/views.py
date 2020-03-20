f2dbe4a718008b39c1d3db2c9ca46256bc9be98e
SusanStoHelit <62446344+SusanStoHelit@users.noreply.github.com>
2020-03-20 20:36:02 +0000

    Initial Commit

---
+++ contacts_book/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,redirect
+
+def home(request):
+	return render(request, 'home.html',{})
+
+def add_contact(request):
+	return render(request, 'add_contact.html',{})
