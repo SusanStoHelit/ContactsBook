a31ae366897e71a7279f31d5d61d60fb13f36463
SusanStoHelit <62446344+SusanStoHelit@users.noreply.github.com>
2020-04-19 16:43:43 +0100

    Adding validation for Contacts

--- contacts_book/models.py
+++ contacts_book/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator, EmailValidator
 
 class Contacts(models.Model):
-	name = models.CharField(max_length=200)
-	mail = models.CharField(max_length=200)
-	phone = models.CharField(max_length=15)
-	address = models.CharField(max_length=200)
-	city = models.CharField(max_length=100)
-	country = models.CharField(max_length=100)
-	postalcode = models.CharField(max_length=10)
+	alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+	val_phone = RegexValidator(r'^[\+|0-9][0-9]*$','Only Number or international format e.g +3531234567')
+	val_email = EmailValidator('Invalid Email address.')
+	name = models.CharField(max_length=200, validators=[alphanumeric])
+	mail = models.CharField(max_length=200, validators=[val_email])
+	phone = models.CharField(max_length=15, validators=[val_phone])
+	address = models.CharField(max_length=200, validators=[alphanumeric])
+	city = models.CharField(max_length=100, validators=[alphanumeric])
+	country = models.CharField(max_length=100, validators=[alphanumeric])
+	postalcode = models.CharField(max_length=10, validators=[alphanumeric])
 	userowner = models.CharField(max_length=100)
 
 	def __str__(self):
